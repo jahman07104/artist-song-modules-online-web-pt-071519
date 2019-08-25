2002a49910609199b26856ce86842b74c0813865
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 19:41:17 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,7 +4,9 @@ module Memorable
     def find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end  
-      
+     def find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end 
       
       
     end
