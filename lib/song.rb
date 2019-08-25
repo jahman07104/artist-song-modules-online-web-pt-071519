15868983fedbc58e90efaa212101c8e7a35b1822
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 20:07:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,8 +23,8 @@ include Memorable::ClassMethods
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
   
 end
